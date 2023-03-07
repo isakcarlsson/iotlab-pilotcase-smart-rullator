a814053933275f2cc5b8258ed7071cde5802cac4
@dl222qb <dl222qb@student.lnu.se>
2023-03-07 09:55:05 +0100

    freertos and hallsensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include "M5Atom.h"
 #include <math.h>
 
+#define HALL 32
+int value = 1;
+int cycle = 0;
+int check = 0;
+
 // sudo chmod a+rw /dev/ttyUSB0
 
 float acc0[] = {0.0f, 0.0f, 0.0f};
@@ -47,69 +52,97 @@ void projectVector(float A[], float B[], int n, float C[]) {
   }
 }
 
+void accel(void * pvParameters) {
+   while (1) {
+    M5.IMU.getAccelData(&acc[0], &acc[1], &acc[2]);
+    acc[0] = acc[0] - acc0[0];
+    acc[1] = acc[1] - acc0[1];
+    acc[2] = acc[2] - acc0[2];
+    
+    if (magnitude(acc, 3) > 0.022) {
+      vectors[0] = acc[0];
+      vectors[1] = acc[1];
+      vectors[2] = acc[2];
+      vectorCounter++;
+
+      if (millis() - lastTime > 100) {
+        
+        vectors[0] = vectors[0] / vectorCounter;
+        vectors[1] = vectors[1] / vectorCounter;
+        vectors[2] = vectors[2] / vectorCounter;
+
+        if (magnitude(prevAcc, 3) == 0.0f) {
+          prevAcc[0] = vectors[0];
+          prevAcc[1] = vectors[1];
+          prevAcc[2] = vectors[2];    
+        }
+
+        float projection[3] = {0.0f, 0.0f, 0.0f};
+        projectVector(prevAcc, acc, 3, projection);
+
+        unsigned int angle = round(angleBetweenVectors(acc, prevAcc, 3));
+        float projectionMag = magnitude(projection, 3);
+        
+        if (angle > 90 && projectionMag > 0.014) {
+          prevAcc[0] = acc[0];
+          prevAcc[1] = acc[1];
+          prevAcc[2] = acc[2];   
+
+          accelerations++;
+          steps = accelerations / 2;
+
+          if (accelerations % 2 == 0) {
+            Serial.printf("%u steps\n", steps);
+          }
+        
+          vTaskDelay(pdMS_TO_TICKS(100));
+        }
+
+        vectorCounter = 0;
+        lastTime = millis();
+      }
+    }
+  }
+}
+
+
+void hall(void * pvParameters) {
+  while (1) {
+    value = digitalRead(HALL);
+
+    if (value == 0) {
+        if (check == 0) {
+            check = 1;
+            cycle++;
+            Serial.printf("cycle:%d\n", cycle);
+            vTaskDelay(pdMS_TO_TICKS(100));
+        }
+    } 
+    else {
+      check = 0;
+    }
+  }
+}
+
 void setup(){
-  M5.begin(true, false, true);
+  M5.begin(true,false,true);
   delay(100);
   M5.IMU.Init();
   delay(100);
   M5.IMU.SetAccelFsr(M5.IMU.AFS_16G);
   delay(100);
-
+  pinMode (HALL, INPUT);
   M5.IMU.getAccelData(&acc[0], &acc[1], &acc[2]);
   acc0[0] = acc[0];
   acc0[1] = acc[1];
   acc0[2] = acc[2]; 
-
   lastTime = millis();
+
+  xTaskCreatePinnedToCore(accel, "accel", 4096, NULL, 5, NULL, 0);
+  xTaskCreatePinnedToCore(hall, "hall", 4096, NULL, 5, NULL, 1);
+
 }
 
+
 void loop() {
-  M5.IMU.getAccelData(&acc[0], &acc[1], &acc[2]);
-  acc[0] = acc[0] - acc0[0];
-  acc[1] = acc[1] - acc0[1];
-  acc[2] = acc[2] - acc0[2];
-  
-  if (magnitude(acc, 3) > 0.022) {
-    vectors[0] = acc[0];
-    vectors[1] = acc[1];
-    vectors[2] = acc[2];
-    vectorCounter++;
-
-    if (millis() - lastTime > 100) {
-      
-      vectors[0] = vectors[0] / vectorCounter;
-      vectors[1] = vectors[1] / vectorCounter;
-      vectors[2] = vectors[2] / vectorCounter;
-
-      if (magnitude(prevAcc, 3) == 0.0f) {
-        prevAcc[0] = vectors[0];
-        prevAcc[1] = vectors[1];
-        prevAcc[2] = vectors[2];    
-      }
-
-      float projection[3] = {0.0f, 0.0f, 0.0f};
-      projectVector(prevAcc, acc, 3, projection);
-
-      unsigned int angle = round(angleBetweenVectors(acc, prevAcc, 3));
-      float projectionMag = magnitude(projection, 3);
-      
-      if (angle > 90 && projectionMag > 0.014) {
-        prevAcc[0] = acc[0];
-        prevAcc[1] = acc[1];
-        prevAcc[2] = acc[2];   
-
-        accelerations++;
-        steps = accelerations / 2;
-
-        if (accelerations % 2 == 0) {
-          Serial.printf("%u steps\n", steps);
-        }
-      
-        delay(200);            
-      }
-
-      vectorCounter = 0;
-      lastTime = millis();
-    }
-  }
 }
